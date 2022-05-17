7724f3606dec975fb905b4d21fb07e0d7e4f0e20
yuvrajjohal <johalyuvraj@gmail.com>
2022-05-17 00:55:31 -0400

    Add project files.

---
+++ GcdForm.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace EuclideanGcdVersusBruteForceGcd
+{
+    partial class GcdForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Num1TextBox = new System.Windows.Forms.TextBox();
+            this.Num2TextBox = new System.Windows.Forms.TextBox();
+            this.GcdLabel = new System.Windows.Forms.Label();
+            this.EuclideanGcdOverload1Button = new System.Windows.Forms.Button();
+            this.BruteForceGcdOverload1Button = new System.Windows.Forms.Button();
+            this.BruteForceGcdOverload2Button = new System.Windows.Forms.Button();
+            this.EuclideanGcdOverload2Button = new System.Windows.Forms.Button();
+            this.ExecutionTimeLabel = new System.Windows.Forms.Label();
+            this.BruteForceGCDBackgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.ComputingInBackgroundTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Num1TextBox
+            // 
+            this.Num1TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Num1TextBox.Location = new System.Drawing.Point(13, 67);
+            this.Num1TextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Num1TextBox.MaxLength = 19;
+            this.Num1TextBox.Name = "Num1TextBox";
+            this.Num1TextBox.Size = new System.Drawing.Size(193, 26);
+            this.Num1TextBox.TabIndex = 0;
+            this.Num1TextBox.Text = "2145678903";
+            // 
+            // Num2TextBox
+            // 
+            this.Num2TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Num2TextBox.Location = new System.Drawing.Point(214, 67);
+            this.Num2TextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Num2TextBox.MaxLength = 19;
+            this.Num2TextBox.Name = "Num2TextBox";
+            this.Num2TextBox.Size = new System.Drawing.Size(193, 26);
+            this.Num2TextBox.TabIndex = 1;
+            this.Num2TextBox.Text = "1234567890";
+            // 
+            // GcdLabel
+            // 
+            this.GcdLabel.AutoSize = true;
+            this.GcdLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GcdLabel.ForeColor = System.Drawing.Color.Blue;
+            this.GcdLabel.Location = new System.Drawing.Point(8, 202);
+            this.GcdLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.GcdLabel.Name = "GcdLabel";
+            this.GcdLabel.Size = new System.Drawing.Size(90, 25);
+            this.GcdLabel.TabIndex = 2;
+            this.GcdLabel.Text = "gcd label";
+            // 
+            // EuclideanGcdOverload1Button
+            // 
+            this.EuclideanGcdOverload1Button.Location = new System.Drawing.Point(13, 112);
+            this.EuclideanGcdOverload1Button.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.EuclideanGcdOverload1Button.Name = "EuclideanGcdOverload1Button";
+            this.EuclideanGcdOverload1Button.Size = new System.Drawing.Size(123, 65);
+            this.EuclideanGcdOverload1Button.TabIndex = 3;
+            this.EuclideanGcdOverload1Button.Text = "Euclidean GCD Overload 1 (returns \'int\')";
+            this.EuclideanGcdOverload1Button.UseVisualStyleBackColor = true;
+            this.EuclideanGcdOverload1Button.Click += new System.EventHandler(this.EuclideanGcdOverload1Button_Click);
+            // 
+            // BruteForceGcdOverload1Button
+            // 
+            this.BruteForceGcdOverload1Button.Location = new System.Drawing.Point(271, 112);
+            this.BruteForceGcdOverload1Button.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BruteForceGcdOverload1Button.Name = "BruteForceGcdOverload1Button";
+            this.BruteForceGcdOverload1Button.Size = new System.Drawing.Size(128, 65);
+            this.BruteForceGcdOverload1Button.TabIndex = 4;
+            this.BruteForceGcdOverload1Button.Text = "Brute-Force GCD Overload 1 (returns \'int\')";
+            this.BruteForceGcdOverload1Button.UseVisualStyleBackColor = true;
+            this.BruteForceGcdOverload1Button.Click += new System.EventHandler(this.BruteForceGcdOverload1Button_Click);
+            // 
+            // BruteForceGcdOverload2Button
+            // 
+            this.BruteForceGcdOverload2Button.Location = new System.Drawing.Point(407, 112);
+            this.BruteForceGcdOverload2Button.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BruteForceGcdOverload2Button.Name = "BruteForceGcdOverload2Button";
+            this.BruteForceGcdOverload2Button.Size = new System.Drawing.Size(129, 65);
+            this.BruteForceGcdOverload2Button.TabIndex = 6;
+            this.BruteForceGcdOverload2Button.Text = "Brute-Force GCD  Overload 2 (returns \'long\')";
+            this.BruteForceGcdOverload2Button.UseVisualStyleBackColor = true;
+            this.BruteForceGcdOverload2Button.Click += new System.EventHandler(this.BruteForceGcdOverload2Button_Click);
+            // 
+            // EuclideanGcdOverload2Button
+            // 
+            this.EuclideanGcdOverload2Button.Location = new System.Drawing.Point(144, 112);
+            this.EuclideanGcdOverload2Button.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.EuclideanGcdOverload2Button.Name = "EuclideanGcdOverload2Button";
+            this.EuclideanGcdOverload2Button.Size = new System.Drawing.Size(119, 65);
+            this.EuclideanGcdOverload2Button.TabIndex = 5;
+            this.EuclideanGcdOverload2Button.Text = "Euclidean GCD Overload 2 (returns \'long\')";
+            this.EuclideanGcdOverload2Button.UseVisualStyleBackColor = true;
+            this.EuclideanGcdOverload2Button.Click += new System.EventHandler(this.EuclideanGcdOverload2Button_Click);
+            // 
+            // ExecutionTimeLabel
+            // 
+            this.ExecutionTimeLabel.AutoSize = true;
+            this.ExecutionTimeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExecutionTimeLabel.ForeColor = System.Drawing.Color.Blue;
+            this.ExecutionTimeLabel.Location = new System.Drawing.Point(9, 244);
+            this.ExecutionTimeLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.ExecutionTimeLabel.Name = "ExecutionTimeLabel";
+            this.ExecutionTimeLabel.Size = new System.Drawing.Size(178, 24);
+            this.ExecutionTimeLabel.TabIndex = 7;
+            this.ExecutionTimeLabel.Text = "execution time label";
+            // 
+            // BruteForceGCDBackgroundWorker
+            // 
+            this.BruteForceGCDBackgroundWorker.WorkerReportsProgress = true;
+            this.BruteForceGCDBackgroundWorker.WorkerSupportsCancellation = true;
+            this.BruteForceGCDBackgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BruteForceGCDBackgroundWorker_DoWork);
+            this.BruteForceGCDBackgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BruteForceGCDBackgroundWorker_RunWorkerCompleted);
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(544, 112);
+            this.CancelButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(143, 65);
+            this.CancelButton.TabIndex = 8;
+            this.CancelButton.Text = "Cancel Brute-Force Computation of GCD";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // ComputingInBackgroundTimer
+            // 
+            this.ComputingInBackgroundTimer.Interval = 750;
+            this.ComputingInBackgroundTimer.Tick += new System.EventHandler(this.ComputingInBackgroundTimer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(9, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(759, 56);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "This app shows the importance of choosing algorithms. The Euclidean algorithm for" +
+    " computing the greatest common divisor of two integers is clearly much more effi" +
+    "cient than any brute-force method. ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // GcdForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 286);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.ExecutionTimeLabel);
+            this.Controls.Add(this.BruteForceGcdOverload2Button);
+            this.Controls.Add(this.EuclideanGcdOverload2Button);
+            this.Controls.Add(this.BruteForceGcdOverload1Button);
+            this.Controls.Add(this.EuclideanGcdOverload1Button);
+            this.Controls.Add(this.GcdLabel);
+            this.Controls.Add(this.Num2TextBox);
+            this.Controls.Add(this.Num1TextBox);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "GcdForm";
+            this.Text = "Comparing Execution Time of Euclidean and Brute-Force GCD";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Num1TextBox;
+        private System.Windows.Forms.TextBox Num2TextBox;
+        private System.Windows.Forms.Label GcdLabel;
+        private System.Windows.Forms.Button EuclideanGcdOverload1Button;
+        private System.Windows.Forms.Button BruteForceGcdOverload1Button;
+        private System.Windows.Forms.Button BruteForceGcdOverload2Button;
+        private System.Windows.Forms.Button EuclideanGcdOverload2Button;
+        private System.Windows.Forms.Label ExecutionTimeLabel;
+        private System.ComponentModel.BackgroundWorker BruteForceGCDBackgroundWorker;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.Timer ComputingInBackgroundTimer;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ GcdForm.cs
@@ -0,0 +1,396 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EuclideanGcdVersusBruteForceGcd
+{
+    public partial class GcdForm : Form
+    {
+        #region Fields (Global Variables)
+
+        // Used to allow easy enabling/disabling of the 4 gcd buttons.
+        private readonly Button[] gcdButton = new Button[4];
+
+        #endregion
+
+        #region Constructor Method
+        public GcdForm()
+        {
+            InitializeComponent();
+
+            GcdLabel.Text = "";
+            ExecutionTimeLabel.Text = "";
+
+            // Load references to gcd buttons into the array 'gcdButton'
+            gcdButton[0] = EuclideanGcdOverload1Button;
+            gcdButton[1] = BruteForceGcdOverload1Button;
+            gcdButton[2] = EuclideanGcdOverload2Button;
+            gcdButton[3] = BruteForceGcdOverload2Button;
+        }
+        #endregion
+
+        #region Methods for Computing GCD
+        // 'EuclideanGcd' Overload 1:
+        // Uses the Euclidean algorithm to compute the greatest common divisor of 'm' and 'n'
+        // int m, int n, returns int
+
+        private static int EuclideanGcd(int m, int n)
+        {
+            // gcd(0, 0) is undefined -> indicated by returning -1 since gcd(m, n) >= 0
+            int gcd = -1;
+
+            if (m != 0 && n != 0)
+            {
+                // Algorithm only works if m>=0 and n>=0. (gcd(m, n) = gcd(|m|, |n|).)
+                m = Math.Abs(m);
+                n = Math.Abs(n);
+
+                do
+                {
+                    int remainder = m % n;
+                    m = n;
+                    n = remainder;
+                } while (n > 0);
+
+                gcd = m;
+            }
+
+            return gcd; //Keep in mind that the best practice is to have one 'return' at the end.
+        }
+
+        // 'EuclideanGcd' Overload 2:
+        // Uses the Euclidean algorithm to compute the greatest common divisor of 'm' and 'n'
+        // long m, long n, returns long
+
+        private static long EuclideanGcd(long m, long n)
+        {
+            // gcd(0, 0) is undefined -> indicated by returning -1 since gcd(m, n) >= 0
+            long gcd = -1;
+
+            if (m != 0 && n != 0)
+            {
+                // Algorithm only works if m>=0 and n>=0. (gcd(m, n) = gcd(|m|, |n|).)
+                m = Math.Abs(m);
+                n = Math.Abs(n);
+
+                do
+                {
+                    long remainder = m % n;
+                    m = n;
+                    n = remainder;
+                } while (n > 0);
+
+                gcd = m;
+            }
+
+            return gcd; 
+        }
+
+        // 'BruteForceGcd' Overload 1:
+        // Uses exhaustive search to compute the greatest common divisor of 'm' and 'n'
+        // int m, int n, returns int
+        // 'BackgroundWorker' component needed because 'BruteForceGCD' is very slow when 'm' and 'n' are large.
+
+        private static int BruteForceGcd(int m, int n, BackgroundWorker gcdBackgroundWorker)
+        {
+            // gcd(0, 0) is undefined -> indicated by returning -1 since gcd(m, n) >= 0
+            int gcd = -1;
+
+            if (m != 0 && n != 0)
+            {
+                gcd = 1; // If no common divisor > 1 is found, gcd must equal 1.
+
+                // Algorithm only works if m >= 0 and n >= 0. (gcd(m, n) = gcd(|m|, |n|).)
+                m = Math.Abs(m);
+                n = Math.Abs(n);
+
+                int smaller = Math.Min(m, n);
+
+                for (int i = smaller; i >= 2 && !gcdBackgroundWorker.CancellationPending; i--)
+                {
+                    if (m % i == 0 && n % i == 0)
+                    {
+                        gcd = i;
+                        break; // Prematurely exit loop because gcd has been found.
+                    }
+                }
+            }
+
+            return gcd;  
+        }
+
+
+        // 'BruteForceGcd' Overload 2:
+        // Uses exhaustive search to compute the greatest common divisor of 'm' and 'n'
+        // long m, long n, returns long
+        // 'BackgroundWorker' component needed because 'BruteForceGCD' is very slow when 'm' and 'n' are large.
+
+        private static long BruteForceGcd(long m, long n, BackgroundWorker gcdBackgroundWorker)
+        {
+            // gcd(0, 0) is undefined -> indicated by returning -1 since gcd(m, n) >= 0
+            long gcd = -1;
+
+            if (m != 0 && n != 0)
+            {
+                gcd = 1; // If no common divisor > 1 is found, gcd must equal 1.
+
+                // Algorithm only works if m >= 0 and n >= 0. (gcd(m, n) = gcd(|m|, |n|).)
+                m = Math.Abs(m);
+                n = Math.Abs(n);
+
+                long smaller = Math.Min(m, n);
+
+                for (long i = smaller; i >= 2 && !gcdBackgroundWorker.CancellationPending; i--)
+                {
+                    if (m % i == 0 && n % i == 0)
+                    {
+                        gcd = i;
+                        break; // Prematurely exit loop because gcd has been found.
+                    }
+                }
+            }
+
+            return gcd; 
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        private void EuclideanGcdOverload1Button_Click(object sender, EventArgs e)
+        {
+            GcdLabel.Text = "";
+            ExecutionTimeLabel.Text = "";
+
+            bool conversion1Succeeded = int.TryParse(Num1TextBox.Text, out int a);
+            bool conversion2Succeeded = int.TryParse(Num2TextBox.Text, out int b);
+
+            if (conversion1Succeeded && conversion2Succeeded)
+            {
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+
+                int gcd = EuclideanGcd(a, b);
+
+                watch.Stop();
+                var elapsedMs = watch.ElapsedMilliseconds;
+
+                GcdLabel.Text = "gcd(" + a + ", " + b + ") = " + gcd;
+                ExecutionTimeLabel.Text = "'EuclideanGcd' Overload 1: Execution Time = " + elapsedMs + " ms";
+            }
+            else
+            {
+                GcdLabel.Text = "Input(s) invalid.";
+                ExecutionTimeLabel.Text = "At least one of the values entered is outside the range of an 'int' or not numeric.";
+                //MessageBox.Show("At least one of the values entered is outside the range of an 'int' or not numeric.", "Oops!");
+            }
+
+        }
+
+        private void EuclideanGcdOverload2Button_Click(object sender, EventArgs e)
+        {
+            GcdLabel.Text = "";
+            ExecutionTimeLabel.Text = "";
+
+            bool conversion1Succeeded = long.TryParse(Num1TextBox.Text, out long a);
+            bool conversion2Succeeded = long.TryParse(Num2TextBox.Text, out long b);
+
+            if (conversion1Succeeded && conversion2Succeeded)
+            {
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+
+                long gcd = EuclideanGcd(a, b);
+
+                watch.Stop();
+                //var elapsedMs = watch.ElapsedMilliseconds;
+
+                GcdLabel.Text = "gcd(" + a + ", " + b + ") = " + gcd;
+                ExecutionTimeLabel.Text = "'EuclideanGcd' Overload 2: Execution Time = " + watch.ElapsedMilliseconds + " ms";
+            }
+            else
+            {
+                GcdLabel.Text = "Input(s) invalid.";
+                ExecutionTimeLabel.Text = "At least one of the values entered is outside the range of a 'long' or not numeric.";
+            }
+
+
+        }
+
+        private void BruteForceGcdOverload1Button_Click(object sender, EventArgs e)
+        {
+            GcdLabel.Text = "This may take some time. Computing in the background...";
+            ExecutionTimeLabel.Text = "";
+
+            // Disable the gcd buttons. 
+            // Clicking these buttons can cause problems while an asynchronous operation is running.
+            EnableButtons(gcdButton, false);
+            ComputingInBackgroundTimer.Enabled = true;
+
+            // Run the brute-force gcd operation in the background. Note that 'RunWorkerAsync' has two overloads.
+            // The first does not take any arguments. The second takes a single argument that can be used to
+            // pass a message to the 'DoWork' event handler.
+            BruteForceGCDBackgroundWorker.RunWorkerAsync(1); // '1' means overload 1
+        }
+
+        private void BruteForceGcdOverload2Button_Click(object sender, EventArgs e)
+        {
+            GcdLabel.Text = "This may take some time. Computing in the background...";
+            ExecutionTimeLabel.Text = "";
+
+            // Disable the gcd buttons. 
+            // Clicking these buttons can cause problems while an asynchronous operation is running.
+            EnableButtons(gcdButton, false);
+            ComputingInBackgroundTimer.Enabled = true;
+
+            // Run the brute-force gcd operation in the background. Note that 'RunWorkerAsync' has two overloads.
+            // The first does not take any arguments. The second takes a single argument that can be used to
+            // pass a message to the 'DoWork' event handler.
+            BruteForceGCDBackgroundWorker.RunWorkerAsync(2); // '2' means overload 2
+        }
+
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            BruteForceGCDBackgroundWorker.CancelAsync();
+        }
+
+        private void BruteForceGCDBackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int overloadCalled = (int)e.Argument;
+
+            if (overloadCalled == 1)
+            {
+                bool conversion1Succeeded = int.TryParse(Num1TextBox.Text, out int a);
+                bool conversion2Succeeded = int.TryParse(Num2TextBox.Text, out int b);
+
+                if (conversion1Succeeded && conversion2Succeeded)
+                {
+                    // Do not access the form's BackgroundWorker reference directly.
+                    // Instead, use the reference provided by the 'sender' parameter.
+                    BackgroundWorker gcdBackgroundWorker = sender as BackgroundWorker;
+
+                    var watch = System.Diagnostics.Stopwatch.StartNew();
+
+                    int gcd = BruteForceGcd(a, b, gcdBackgroundWorker); // Potentially time-consuming operation.
+
+                    watch.Stop();
+
+                    string timeTakenMessage = "'BruteForceGcd' Overload 1: Execution Time = " + watch.ElapsedMilliseconds + " ms";
+
+                    if (gcd >= 0 && !gcdBackgroundWorker.CancellationPending)
+                    {
+                        e.Result = new string[] { "gcd(" + a + ", " + b + ") = " + gcd, timeTakenMessage };
+                    }
+                    else if (!gcdBackgroundWorker.CancellationPending)
+                    {
+                        e.Result = new string[] { "gcd(0, 0) is undefined", timeTakenMessage };
+                    }
+                    else // 'bw.CancellationPending' must be 'true'
+                    {
+                        // If the operation was cancelled by the user, set the 'DoWorkEventArgs.Cancel' property to true.
+                        e.Cancel = true;
+                    }
+                }
+                else
+                {
+                    e.Result = new string[] { "Input(s) invalid.", "At least one of the values entered is outside the range of an 'int' or not numeric." };
+                }
+            }
+            else if (overloadCalled == 2)
+            {
+                bool conversion1Succeeded = long.TryParse(Num1TextBox.Text, out long a);
+                bool conversion2Succeeded = long.TryParse(Num2TextBox.Text, out long b);
+
+                if (conversion1Succeeded && conversion2Succeeded)
+                {
+                    // Do not access the form's BackgroundWorker reference directly.
+                    // Instead, use the reference provided by the 'sender' parameter.
+                    BackgroundWorker bw = sender as BackgroundWorker;
+
+                    var watch = System.Diagnostics.Stopwatch.StartNew();
+
+                    long gcd = BruteForceGcd(a, b, bw); // Potentially time-consuming operation.
+
+                    watch.Stop();
+
+                    string timeTakenMessage = "'BruteForceGcd' Overload 2: Execution Time = " + watch.ElapsedMilliseconds + " ms";
+
+                    if (gcd >= 0 && !bw.CancellationPending)
+                    {
+                        e.Result = new string[] { "gcd(" + a + ", " + b + ") = " + gcd, timeTakenMessage };
+                    }
+                    else if (!bw.CancellationPending)
+                    {
+                        e.Result = new string[] { "gcd(0, 0) is undefined", timeTakenMessage };
+                    }
+                    else // 'bw.CancellationPending' must be 'true'
+                    {
+                        // If the operation was cancelled by the user, set the 'DoWorkEventArgs.Cancel' property to true.
+                        e.Cancel = true;
+                    }
+                }
+                else
+                {
+                    e.Result = new string[] { "Input(s) invalid.", "At least one of the values entered is outside the range of a 'long' or not numeric." };
+                }
+            }
+
+        }
+
+
+        private void BruteForceGCDBackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            ComputingInBackgroundTimer.Enabled = false;
+            GcdLabel.Visible = true;
+            EnableButtons(gcdButton, true); // Safe to enable the buttons now.
+
+            if (e.Cancelled)
+            {
+                // The user cancelled the operation.
+                GcdLabel.Text = "Brute-force computation of GCD was aborted.";
+                ExecutionTimeLabel.Text = "";
+            }
+            else
+            {
+                string[] result = (string[])e.Result;
+                GcdLabel.Text = result[0];
+                ExecutionTimeLabel.Text = result[1];
+            }
+        }
+
+        // Used to produce the flashing effect when brute-force gcd takes a long time to return a result.
+        private void ComputingInBackgroundTimer_Tick(object sender, EventArgs e)
+        {
+            if (GcdLabel.Visible)
+            {
+                GcdLabel.Visible = false;
+            }
+            else
+            {
+                GcdLabel.Visible = true;
+            }
+        }
+
+        #endregion
+
+        #region Miscellaneous Methods
+        private void EnableButtons(Button[] button, bool enabled)
+        {
+            for (int i = 0; i < button.Length; i++)
+            {
+                button[i].Enabled = enabled;
+            }
+        }
+        #endregion
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
